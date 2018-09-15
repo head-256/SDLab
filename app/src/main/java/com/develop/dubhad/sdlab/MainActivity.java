dfe97e49cc395544efc8a4cc0116861c91b2f2c0
Dubhad <dubhad.connect@yandex.ru>
2018-09-15 19:12:42 +0300

    Show app version

--- app/src/main/java/com/develop/dubhad/sdlab/MainActivity.java
+++ app/src/main/java/com/develop/dubhad/sdlab/MainActivity.java
@@ -2,12 +2,18 @@ package com.develop.dubhad.sdlab;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView appVersionView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appVersionView = findViewById(R.id.appVersionView);
+        appVersionView.setText(BuildConfig.VERSION_NAME);
     }
 }
