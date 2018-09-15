6073cc9d92922255b5044fdb0023518108672e98
Dubhad <dubhad.connect@yandex.ru>
2018-09-15 22:10:09 +0300

    Refactoring

--- app/src/main/java/com/develop/dubhad/sdlab/AbstractPermissionActivity.java
+++ app/src/main/java/com/develop/dubhad/sdlab/AbstractPermissionActivity.java
@@ -2,57 +2,51 @@ package com.develop.dubhad.sdlab;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
-abstract public class AbstractPermissionActivity
-        extends AppCompatActivity {
+abstract public class AbstractPermissionActivity extends AppCompatActivity {
+
     abstract protected String[] getDesiredPermissions();
+
     abstract protected void onPermissionDenied();
+
     abstract protected void onReady(Bundle state);
 
-    private static final int REQUEST_PERMISSION=61125;
-    private static final String STATE_IN_PERMISSION="inPermission";
-    private boolean isInPermission=false;
+    private static final int REQUEST_PERMISSION = 61125;
+    private static final String STATE_IN_PERMISSION = "inPermission";
+    private boolean isInPermission = false;
     private Bundle state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.state = savedInstanceState;
 
-        this.state=savedInstanceState;
-
-        if (state!=null) {
-            isInPermission=state.getBoolean(STATE_IN_PERMISSION, false);
+        if (state != null) {
+            isInPermission = state.getBoolean(STATE_IN_PERMISSION, false);
         }
 
         if (hasAllPermissions(getDesiredPermissions())) {
             onReady(state);
-        }
-        else if (!isInPermission) {
-            isInPermission=true;
-
-            ActivityCompat
-                    .requestPermissions(this,
-                            netPermissions(getDesiredPermissions()),
-                            REQUEST_PERMISSION);
+        } else if (!isInPermission) {
+            isInPermission = true;
+            ActivityCompat.requestPermissions(this, getUnavailablePermissions(getDesiredPermissions()), REQUEST_PERMISSION);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions,
-                                           int[] grantResults) {
-        isInPermission=false;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        isInPermission = false;
 
-        if (requestCode==REQUEST_PERMISSION) {
+        if (requestCode == REQUEST_PERMISSION) {
             if (hasAllPermissions(getDesiredPermissions())) {
                 onReady(state);
-            }
-            else {
+            } else {
                 onPermissionDenied();
             }
         }
@@ -65,30 +59,29 @@ abstract public class AbstractPermissionActivity
         outState.putBoolean(STATE_IN_PERMISSION, isInPermission);
     }
 
-    private boolean hasAllPermissions(String[] perms) {
-        for (String perm : perms) {
-            if (!hasPermission(perm)) {
-                return(false);
+    private boolean hasAllPermissions(String[] permissions) {
+        for (String permission : permissions) {
+            if (!hasPermission(permission)) {
+                return false;
             }
         }
 
-        return(true);
+        return true;
     }
 
-    protected boolean hasPermission(String perm) {
-        return(ContextCompat.checkSelfPermission(this, perm)==
-                PackageManager.PERMISSION_GRANTED);
+    protected boolean hasPermission(String permission) {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
     }
 
-    private String[] netPermissions(String[] wanted) {
-        ArrayList<String> result=new ArrayList<String>();
+    private String[] getUnavailablePermissions(String[] wantedPermissions) {
+        ArrayList<String> unavailablePermissions = new ArrayList<>();
 
-        for (String perm : wanted) {
-            if (!hasPermission(perm)) {
-                result.add(perm);
+        for (String permission : wantedPermissions) {
+            if (!hasPermission(permission)) {
+                unavailablePermissions.add(permission);
             }
         }
 
-        return(result.toArray(new String[result.size()]));
+        return unavailablePermissions.toArray(new String[unavailablePermissions.size()]);
     }
 }
--- app/src/main/java/com/develop/dubhad/sdlab/MainActivity.java
+++ app/src/main/java/com/develop/dubhad/sdlab/MainActivity.java
@@ -3,6 +3,7 @@ package com.develop.dubhad.sdlab;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
@@ -12,32 +13,43 @@ public class MainActivity extends AbstractPermissionActivity {
 
     private static final String[] PERMS = {Manifest.permission.READ_PHONE_STATE};
 
-    private TextView appVersionView;
-    private TextView imeiView;
-
     @Override
     protected String[] getDesiredPermissions() {
-        return(PERMS);
+        return PERMS;
     }
 
     @Override
     protected void onPermissionDenied() {
-        Toast
-                .makeText(this, "No permissions", Toast.LENGTH_LONG)
-                .show();
+        Toast.makeText(this, "No permissions", Toast.LENGTH_LONG).show();
         finish();
     }
 
-    @SuppressLint("MissingPermission")
     @Override
     protected void onReady(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
 
-        appVersionView = findViewById(R.id.appVersionView);
-        appVersionView.setText(BuildConfig.VERSION_NAME);
+        TextView appVersionView = findViewById(R.id.appVersionView);
+        TextView imeiView = findViewById(R.id.imeiView);
 
+        appVersionView.setText(getAppVersionName());
+        imeiView.setText(getImei());
+    }
+
+    private String getAppVersionName() {
+        return BuildConfig.VERSION_NAME;
+    }
+
+    @SuppressLint({"MissingPermission", "HardwareIds"})
+    private String getImei() {
+        String imei = null;
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        imeiView = findViewById(R.id.imeiView);
-        imeiView.setText(telephonyManager.getDeviceId());
+        if (telephonyManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                imei = telephonyManager.getImei();
+            } else {
+                imei = telephonyManager.getDeviceId();
+            }
+        }
+        return imei;
     }
 }
