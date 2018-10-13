a380e767dfe005b58d60cd2cb9004cddeb8d500e
Dubhad <dubhad.connect@yandex.ru>
2018-10-13 20:08:21 +0300

    Requesting permissions rewrite

--- app/src/main/java/com/develop/dubhad/sdlab/MainActivity.java
+++ app/src/main/java/com/develop/dubhad/sdlab/MainActivity.java
@@ -2,37 +2,76 @@ package com.develop.dubhad.sdlab;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AbstractPermissionActivity {
+public class MainActivity extends AppCompatActivity {
 
-    private static final String[] PERMS = {Manifest.permission.READ_PHONE_STATE};
+    private static final int REQUEST_READ_PHONE_STATE = 111;
 
-    @Override
-    protected String[] getDesiredPermissions() {
-        return PERMS;
-    }
+    private TextView imeiView;
 
     @Override
-    protected void onPermissionDenied() {
-        Toast.makeText(this, "No permissions", Toast.LENGTH_LONG).show();
-        finish();
-    }
-
-    @Override
-    protected void onReady(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView appVersionView = findViewById(R.id.appVersionView);
-        TextView imeiView = findViewById(R.id.imeiView);
-
         appVersionView.setText(getAppVersionName());
-        imeiView.setText(getImei());
+
+        imeiView = findViewById(R.id.imeiView);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE)) {
+                showPermissionRationale(this, new String[]{Manifest.permission.READ_PHONE_STATE},
+                        REQUEST_READ_PHONE_STATE, getString(R.string.read_phone_state_rationale));
+            }
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE},
+                        REQUEST_READ_PHONE_STATE);
+            }
+        } else {
+            imeiView.setText(getImei());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_PHONE_STATE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, getString(R.string.permission_granted), Toast.LENGTH_SHORT).show();
+                    imeiView.setText(getImei());
+                }
+            }
+        }
+    }
+
+    private void showPermissionRationale(final Activity context, final String[] permissions, final int requestCode,
+                                         String message) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle(getString(R.string.permission_necessary));
+        alertBuilder.setMessage(message);
+        alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                ActivityCompat.requestPermissions(context, permissions, requestCode);}});
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
     }
 
     private String getAppVersionName() {
