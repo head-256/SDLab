e62dc0960ef75580512eab6d5e1e0709b42d46d6
Dubhad <dubhad.connect@yandex.ru>
2018-10-13 23:18:25 +0300

    Basiv Navigation Drawer added

--- app/src/main/java/com/develop/dubhad/sdlab/MainActivity.java
+++ app/src/main/java/com/develop/dubhad/sdlab/MainActivity.java
@@ -8,10 +8,18 @@ import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.telephony.TelephonyManager;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import static com.develop.dubhad.sdlab.Util.PermissionUtil.hasPermission;
 import static com.develop.dubhad.sdlab.Util.PermissionUtil.requestPermission;
@@ -19,15 +27,47 @@ import static com.develop.dubhad.sdlab.Util.PermissionUtil.requestPermission;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_READ_PHONE_STATE = 111;
+    private static final String PROFILE_FRAGMENT_TAG = "profile_fragment";
 
     private TextView imeiView;
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView appVersionView = findViewById(R.id.appVersionView);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.profile:
+                                if (getSupportFragmentManager().findFragmentById(R.id.content_frame) == null) {
+                                    getSupportFragmentManager().beginTransaction().add(R.id.content_frame, new ProfileFragment(),
+                                            PROFILE_FRAGMENT_TAG).commit();
+                                }
+                                return true;
+                        }
+
+                        return true;
+                    }
+                });
+
+        /*TextView appVersionView = findViewById(R.id.appVersionView);
         appVersionView.setText(getAppVersionName());
 
         imeiView = findViewById(R.id.imeiView);
@@ -37,7 +77,17 @@ public class MainActivity extends AppCompatActivity {
                     getString(R.string.read_phone_state_rationale));
         } else {
             imeiView.setText(getImei());
+        }*/
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -46,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
             case REQUEST_READ_PHONE_STATE: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, getString(R.string.permission_granted), Toast.LENGTH_SHORT).show();
-                    imeiView.setText(getImei());
+                    //imeiView.setText(getImei());
                 }
             }
         }
