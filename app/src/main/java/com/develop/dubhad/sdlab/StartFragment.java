26a140e587b50d4380cc1af8ebef9fdb086401e3
Dubhad <dubhad.connect@yandex.ru>
2018-10-14 18:06:13 +0300

    Added basic deep linking

--- app/src/main/java/com/develop/dubhad/sdlab/StartFragment.java
+++ app/src/main/java/com/develop/dubhad/sdlab/StartFragment.java
@@ -1,21 +1,17 @@
 package com.develop.dubhad.sdlab;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 
 public class StartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false);
     }
 }
