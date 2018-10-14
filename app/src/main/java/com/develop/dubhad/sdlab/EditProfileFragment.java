79d14e85f820a22f618d09054e5f027659e9aec9
Dubhad <dubhad.connect@yandex.ru>
2018-10-14 17:03:46 +0300

    Edit profile feature added

--- app/src/main/java/com/develop/dubhad/sdlab/EditProfileFragment.java
+++ app/src/main/java/com/develop/dubhad/sdlab/EditProfileFragment.java
@@ -1,18 +1,81 @@
 package com.develop.dubhad.sdlab;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 public class EditProfileFragment extends Fragment {
+
+    private EditText nameEditView;
+    private EditText surnameEditView;
+    private EditText phoneNumberEditView;
+    private EditText emailEditView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.edit_profile_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        nameEditView = view.findViewById(R.id.nameEditView);
+        surnameEditView = view.findViewById(R.id.surnameEditView);
+        phoneNumberEditView = view.findViewById(R.id.phoneNumberEditView);
+        emailEditView = view.findViewById(R.id.emailEditView);
+
+        FloatingActionButton confirmButton = view.findViewById(R.id.confirmButton);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveProfileData();
+                Navigation.findNavController(view).navigateUp();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        fillEditProfileData();
+    }
+
+    private void saveProfileData() {
+        SharedPreferences sharedPref = Objects.requireNonNull(getActivity()).getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.name_field_key), nameEditView.getText().toString());
+        editor.putString(getString(R.string.surname_field_key), surnameEditView.getText().toString());
+        editor.putString(getString(R.string.phone_field_key), phoneNumberEditView.getText().toString());
+        editor.putString(getString(R.string.email_field_key), emailEditView.getText().toString());
+        editor.apply();
+    }
+
+    private void fillEditProfileData() {
+        SharedPreferences sharedPref = Objects.requireNonNull(getActivity()).getPreferences(Context.MODE_PRIVATE);
+        String name = sharedPref.getString(getString(R.string.name_field_key), "");
+        String surname = sharedPref.getString(getString(R.string.surname_field_key), "");
+        String phone = sharedPref.getString(getString(R.string.phone_field_key), "");
+        String email = sharedPref.getString(getString(R.string.email_field_key), "");
+
+        nameEditView.setText(name);
+        surnameEditView.setText(surname);
+        phoneNumberEditView.setText(phone);
+        emailEditView.setText(email);
     }
 }
--- app/src/main/java/com/develop/dubhad/sdlab/ProfileFragment.java
+++ app/src/main/java/com/develop/dubhad/sdlab/ProfileFragment.java
@@ -1,11 +1,16 @@
 package com.develop.dubhad.sdlab;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +18,12 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 public class ProfileFragment extends Fragment {
+
+    private TextView nameView;
+    private TextView surnameView;
+    private TextView phoneNumberView;
+    private TextView emailView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,6 +34,11 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        nameView = view.findViewById(R.id.nameView);
+        surnameView = view.findViewById(R.id.surnameView);
+        phoneNumberView = view.findViewById(R.id.phoneNumberView);
+        emailView = view.findViewById(R.id.emailView);
+
         FloatingActionButton fab = view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +46,25 @@ public class ProfileFragment extends Fragment {
                 Navigation.findNavController(view).navigate(R.id.editProfileFragment);
             }
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        fillProfileData();
+    }
+
+    private void fillProfileData() {
+        SharedPreferences sharedPref = Objects.requireNonNull(getActivity()).getPreferences(Context.MODE_PRIVATE);
+        String name = sharedPref.getString(getString(R.string.name_field_key), "");
+        String surname = sharedPref.getString(getString(R.string.surname_field_key), "");
+        String phone = sharedPref.getString(getString(R.string.phone_field_key), "");
+        String email = sharedPref.getString(getString(R.string.email_field_key), "");
+
+        nameView.setText(name);
+        surnameView.setText(surname);
+        phoneNumberView.setText(phone);
+        emailView.setText(email);
     }
 }
