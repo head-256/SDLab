2d6f5c7197d729584f2db3d790da0caeae6cef3b
Dubhad <dubhad.connect@yandex.ru>
2018-10-14 18:14:06 +0300

    Added two empty fragments

---
+++ app/src/main/java/com/develop/dubhad/sdlab/SecondEmptyFragment.java
@@ -0,0 +1,17 @@
+package com.develop.dubhad.sdlab;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SecondEmptyFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_second_empty, container, false);
+    }
+}
