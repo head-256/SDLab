d5ba7c00d091a0a2476ad64862f4de9f218a0906
Dubhad <dubhad.connect@yandex.ru>
2018-10-15 11:34:21 +0300

    Blank fragments refactoring

--- app/src/main/java/com/develop/dubhad/sdlab/FirstEmptyFragment.java
+++ app/src/main/java/com/develop/dubhad/sdlab/FirstEmptyFragment.java
@@ -5,13 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 
 public class FirstEmptyFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_first_empty, container, false);
     }
--- app/src/main/java/com/develop/dubhad/sdlab/StartFragment.java
+++ app/src/main/java/com/develop/dubhad/sdlab/StartFragment.java
@@ -7,32 +7,37 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import java.util.Objects;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 
 public class StartFragment extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
         setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_start, container, false);
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        getActivity().getMenuInflater().inflate(R.menu.action_menu, menu);
+        Objects.requireNonNull(getActivity()).getMenuInflater().inflate(R.menu.action_menu, menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.infoFragment:
-                Navigation.findNavController(getView()).navigate(R.id.infoFragment);
+                Navigation.findNavController(Objects.requireNonNull(getView())).navigate(R.id.infoFragment);
                 return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
