34af20783d3de54902fc9c2595709614de030cd8
Dubhad <dubhad.connect@yandex.ru>
2018-11-21 13:56:39 +0300

    Room data model added

---
+++ app/src/main/java/com/develop/dubhad/sdlab/models/User.java
@@ -0,0 +1,56 @@
+package com.develop.dubhad.sdlab.models;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    
+    @NonNull
+    private String name;
+    
+    private String surname;
+    
+    private String phoneNumber;
+    
+    private String email;
+    
+    private String picture;
+
+    public User(@NonNull String name, String surname, String phoneNumber, String email, String picture) {
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+}
